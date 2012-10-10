dcae681d935691638d002b08e1e129fd91d51860
wollowizard <alfredo.scaccialepre@gmail.com>
2012-10-10 19:55:16 +0200

    acceptance test created

---
+++ test/cicTest/acceptance/GUIComplexPriorityTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cicTest.acceptance;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author alfredo
+ */
+public class GUIComplexPriorityTest {
+    
+    public GUIComplexPriorityTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
