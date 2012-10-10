8c8fba9fec9f159c8c042aa60c4162eaedcef746
wollowizard <alfredo.scaccialepre@gmail.com>
2012-10-10 19:56:21 +0200

    acceptance test populated and cleaned

--- test/cicTest/acceptance/GUIComplexPriorityTest.java
+++ test/cicTest/acceptance/GUIComplexPriorityTest.java
@@ -4,6 +4,7 @@
  */
 package cicTest.acceptance;
 
+import cic.Cic;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,14 +21,21 @@ public class GUIComplexPriorityTest {
     
     @Before
     public void setUp() {
+        Cic.populate();
+        
     }
     
     @After
     public void tearDown() {
+        
+        Cic.clean();
     }
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void checkPriorityComplexCase() {
+        
+    
+    }
 }
